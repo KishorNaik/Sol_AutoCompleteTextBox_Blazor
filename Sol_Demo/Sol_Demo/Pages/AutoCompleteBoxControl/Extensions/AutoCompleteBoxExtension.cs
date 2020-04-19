73d6facab163777d9da6cb2efe9cbe071ee6d787
KishorNaik <kishor.naik011.net@gmail.com>
2020-04-19 22:01:41 +0530

    Code Commit

---
+++ Sol_Demo/Sol_Demo/Pages/AutoCompleteBoxControl/Extensions/AutoCompleteBoxExtension.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.JSInterop;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Sol_Demo.Pages.AutoCompleteBoxControl.Extensions
+{
+    public static class AutoCompleteBoxExtension
+    {
+        public static async Task AutoCompleteBoxInitAsync(this ElementReference elementReference, IJSRuntime jsRuntime,string autoCompleteBoxContentJson)
+        {
+            await jsRuntime.InvokeVoidAsync(
+                    "autoCompleteBoxInit", elementReference,autoCompleteBoxContentJson);
+        }
+    }
+}
---
+++ Sol_Demo/Sol_Demo/wwwroot/js/autoCompleteBox.js
@@ -0,0 +1,28 @@
+﻿function autoCompleteBoxInit(elementReference, jsonAutoCompleteValue) {
+    console.log(elementReference);
+    console.log(jsonAutoCompleteValue);
+
+    let autoCompleteBoxModel = JSON.parse(jsonAutoCompleteValue);
+    console.log(autoCompleteBoxModel);
+
+    let itemSourceObj = JSON.parse(autoCompleteBoxModel.ItemSourceJson);
+    console.log(itemSourceObj);
+
+  
+    $(document).ready(function () {
+
+        console.log("Jquery Load");
+
+        //$(elementReference).css({ "background-color": "red" });
+        $(elementReference).autocomplete({
+            minLength: autoCompleteBoxModel.MinLength,
+            source: itemSourceObj,
+            select: function (event, ui) {
+                console.log(ui.item.value);
+
+                DotNet.invokeMethodAsync('Sol_Demo', 'OnItemSelectedJS', ui.item.value);
+            }
+        });
+
+    });
+}
